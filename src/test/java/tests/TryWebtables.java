499c1910fdf9c0bd7d9df91235c7e8fc3cc377c3
Vanya2894 <vanya-khlypalo@yandex.ru>
2026-04-14 17:48:10 +0300

    new test for ebables

--- src/test/java/tests/TryWebtables.java
+++ src/test/java/tests/TryWebtables.java
@@ -5,6 +5,9 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.*;
 import org.junit.jupiter.api.*;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class TryWebtables {
 
     Playwright playwright = Playwright.create();
@@ -24,8 +27,39 @@ public class TryWebtables {
                 .setWaitUntil(WaitUntilState
                         .DOMCONTENTLOADED));
 
+        createTableString();
 
 
+
+
+
+    }
+
+    public static String generateRandomString(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+
+    public static String randomInt(){
+        String value =Integer.toString(ThreadLocalRandom.current().nextInt(1, 1001));
+        return value;
+    }
+
+
+    public void createTableString(){
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("First Name")).fill(generateRandomString(10));
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Last Name")).fill(generateRandomString(10));
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("name@example.com")).fill(generateRandomString(3) + "@" + generateRandomString(5) +".ru");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Age")).fill(randomInt());
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Salary")).fill(randomInt());
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Department")).fill(generateRandomString(10));
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit")).click();
     }
 }
 
