81982ba9ced75745d459b60e5c055ece6ae8da55
Vanya2894 <vanya-khlypalo@yandex.ru>
2026-04-15 23:22:50 +0300

    дорабатываю кейс по таблицам

--- src/test/java/tests/TryWebtables.java
+++ src/test/java/tests/TryWebtables.java
@@ -3,10 +3,14 @@ package tests;
 import Pages.Elements.Webtables;
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.*;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class TryWebtables {
 
@@ -15,22 +19,60 @@ public class TryWebtables {
             .launch(new BrowserType
                     .LaunchOptions()
                     .setHeadless(false)
-                    .setSlowMo(250));
-    Page page = browser.newPage();
+                    .setSlowMo(1));
+    BrowserContext context = browser.newContext(
+            new Browser.NewContextOptions()
+                    .setViewportSize(1920, 920)  // ширина, высота в пикселях
+    );
+
+    Page page = context.newPage();
     Webtables webtables = new Webtables();
 
-
+    @Epic("Веб-интерфейс")
+    @Feature("Авторизация")
     @Test
-    public void ThyToChengeValueTest() {
+    public void ThyToChangeValueTest() {
         page.navigate(webtables.getUrl(), new Page
                 .NavigateOptions()
                 .setWaitUntil(WaitUntilState
                         .DOMCONTENTLOADED));
 
-        for (int i = 0; i < 20; i++) {
-            createTableString();
-        }
+        Allure.step("Редактирование и удаление строки таблицы", () -> {
+            page.locator("#edit-record-1 > svg > path").click();
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("First Name")).fill("A ИЗМЕНЕНО!!!");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit")).click();
+            assertThat(page.locator("#root table tr")).hasCount(4);
+            assertThat(page.locator("#root table > tbody > tr:nth-child(1) > td:nth-child(1)")).hasText("A ИЗМЕНЕНО!!!");
+//            page.locator("#delete-record-3 > svg > path").click();
+//              ПОНЯТЬ КАК ПРОВЕРИТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//            assertThat(page.locator("#root table tr")).hasCount(3);
+        });
 
+
+        Allure.step("Поиск по таблице", () -> {
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Type to search")).fill("A ИЗМЕНЕНО!!!");
+            assertThat(page.locator("#root  table tr")).hasCount(2);
+            assertThat(page.locator("#root tr > td:nth-child(1)")).hasText("A ИЗМЕНЕНО!!!");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Type to search")).clear();
+        });
+
+
+        Allure.step("Пагинация", () -> {
+            for (int i = 0; i < 20; i++) {
+                createTableString();
+            }
+            assertThat(page.locator("#root div.pagination.d-flex.align-items-center.justify-content-between.mt-3 div:nth-child(2)")).hasText("Page 1 of 3");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Next")).click();
+            assertThat(page.locator("#root div.pagination.d-flex.align-items-center.justify-content-between.mt-3 div:nth-child(2)")).hasText("Page 2 of 3");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Last")).click();
+            assertThat(page.locator("#root div.pagination.d-flex.align-items-center.justify-content-between.mt-3 div:nth-child(2)")).hasText("Page 3 of 3");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Previous")).click();
+            assertThat(page.locator("#root div.pagination.d-flex.align-items-center.justify-content-between.mt-3 div:nth-child(2)")).hasText("Page 2 of 3");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("First")).click();
+            assertThat(page.locator("#root div.pagination.d-flex.align-items-center.justify-content-between.mt-3 div:nth-child(2)")).hasText("Page 1 of 3");
+        });
+
+        page.waitForTimeout(10000);
 
 
     }
@@ -45,17 +87,17 @@ public class TryWebtables {
         return sb.toString();
     }
 
-    public static String randomInt(){
-        String value =Integer.toString(ThreadLocalRandom.current().nextInt(1, 1001));
+    public static String randomInt() {
+        String value = Integer.toString(ThreadLocalRandom.current().nextInt(1, 1001));
         return value;
     }
 
 
-    public void createTableString(){
+    public void createTableString() {
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add")).click();
         page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("First Name")).fill(generateRandomString(10));
         page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Last Name")).fill(generateRandomString(10));
-        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("name@example.com")).fill(generateRandomString(3) + "@" + generateRandomString(5) +".ru");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("name@example.com")).fill(generateRandomString(3) + "@" + generateRandomString(5) + ".ru");
         page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Age")).fill(randomInt());
         page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Salary")).fill(randomInt());
         page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Department")).fill(generateRandomString(10));
